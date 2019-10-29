2a3b7a1fe15e568d167be4ef7f8d8b0a1be59ba7
irsafam_result <zaqSWE@123!@#ifiresult>
2019-10-29 22:36:45 +0330

    Change web.route to redirect to index page for any request

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+//Route::get('/{any}',function() {
+//	return view('index');
+//});
+
 Route::get('/{any}',function() {
 	return view('index');
-});
-
+})->where('any','.*');
